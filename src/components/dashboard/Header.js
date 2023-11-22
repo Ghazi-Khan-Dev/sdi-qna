4eee2c4e3bd8e1877866b815e6dd25769ae7ac28
Ghazi Khan <mailme.gkhan@gmail.com>
2023-11-22 23:38:22 +0530

    feat : home paage intermediate changes

---
+++ src/components/dashboard/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+    Testing
+    </div>
+  );
+};
+
+export default Header;
--- src/components/dashboard/Home.js
+++ src/components/dashboard/Home.js
@@ -1,6 +1,36 @@
 import React from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import ClearIcon from '@mui/icons-material/Clear';
+
+import "./home.css";
+import Header from "./Header";
+import QuestionAnswer from "../question-answer/QuestionAnswer";
+
+const palceholderForSearch =`type the questions`;
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Header />
+      <div className="home-container">
+
+        {/* search-bar */}
+        <div className="search-bar-container">
+          <span className="search-icon-container">
+            <SearchIcon />
+          </span>
+          <input className="search-bar-input-field" placeholder={palceholderForSearch}/>
+
+          <span className="search-icon-container">
+            <ClearIcon />
+          </span>
+        </div>
+
+        {/* question-answer section */}
+        <QuestionAnswer />
+      </div>
+    </>
+  );
 };
 
 export default Home;
