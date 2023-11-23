8df86bd1a1f4181570477ebdd59953e0814d9367
Ghazi Khan <mailme.gkhan@gmail.com>
2023-11-23 19:17:31 +0530

    Updates

---
+++ precache-manifest.304ad5a662dd7298453c13e969257c2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9be615023d8493dcd6d25151e3fd320",
+    "url": "/sdi-qna/index.html"
+  },
+  {
+    "revision": "65b36ce33dbba598066f",
+    "url": "/sdi-qna/static/css/main.815f7615.chunk.css"
+  },
+  {
+    "revision": "67314fc43f133f6e5040",
+    "url": "/sdi-qna/static/js/2.194a92dc.chunk.js"
+  },
+  {
+    "revision": "8c293dd7af90b8109a8285094c7810df",
+    "url": "/sdi-qna/static/js/2.194a92dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8715a50c396b8261d28c",
+    "url": "/sdi-qna/static/js/3.cbce84d3.chunk.js"
+  },
+  {
+    "revision": "65b36ce33dbba598066f",
+    "url": "/sdi-qna/static/js/main.5bdb67be.chunk.js"
+  },
+  {
+    "revision": "f3127f95d0f19e21c5ec",
+    "url": "/sdi-qna/static/js/runtime-main.26326cc2.js"
+  }
+]);
