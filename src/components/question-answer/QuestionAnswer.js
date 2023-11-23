129d503001be7d558342d742917e82bd798eb926
Ghazi Khan <mailme.gkhan@gmail.com>
2023-11-23 11:05:37 +0530

    feat : added question-answer component

---
+++ src/components/question-answer/QuestionAnswer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./question-answer.css";
+
+const QuestionAnswer = () => {
+  return (
+    <>
+      <div className="question-answer-container">
+      </div>
+    </>
+  );
+};
+
+export default QuestionAnswer;
