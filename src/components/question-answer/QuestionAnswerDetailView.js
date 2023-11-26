cc0cd028978bd8f05eaccbe4c4f9def616f75044
Ghazi Khan <mailme.gkhan@gmail.com>
2023-11-27 00:54:02 +0530

    feat : modal size & possition fix

--- src/components/question-answer/QuestionAnswerDetailView.js
+++ src/components/question-answer/QuestionAnswerDetailView.js
@@ -5,18 +5,41 @@ import Typography from "@mui/joy/Typography";
 import { ModalDialog } from "@mui/joy";
 import { useSelector } from "react-redux";
 
+import './question-answer-detail-view.css'
+
+const DetailedView = (props) => {
+  const { question, answer } = props?.questionanswerDetails;
+  return (
+    <div className="detailed-view-container">
+      <div>
+        <Typography>{`Question - ${question?.statement}`}</Typography>
+      </div>
+      <div>
+        <Typography>{`Answer - ${answer?.statement}`}</Typography>
+      </div>
+    </div>
+  );
+};
+
 const QuestionAnswerDetailView = (props) => {
   const { answerId, handleModalClose } = props;
 
   const questionanswer = useSelector((store) => store.questionanswer);
   const { questionAnswerById } = questionanswer;
+  const questionanswerDetails = questionAnswerById[answerId];
 
   return (
-    <div>
-      <Modal open={answerId !== null} onClose={handleModalClose}>
-        <ModalDialog>
-          <ModalClose />
-          <Typography>{answerId}</Typography>
+    <div className="modal-main-container">
+      <Modal open={answerId !== null} onClose={handleModalClose} >
+        <ModalDialog size="lg" layout="center">
+          {/* <ModalClose /> */}
+          {questionanswerDetails ? (
+            <DetailedView questionanswerDetails={questionanswerDetails}/>
+          ) : (
+            <div>
+              <Typography>No answer available</Typography>
+            </div>
+          )}
         </ModalDialog>
       </Modal>
     </div>
