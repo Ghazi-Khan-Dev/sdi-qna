8ad95243da0628dabf44ffc223f3689970da75a5
Ghazi Khan <mailme.gkhan@gmail.com>
2023-11-26 22:43:45 +0530

    feat : added sdi logo

--- src/components/dashboard/Header.js
+++ src/components/dashboard/Header.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 import './header.css';
+import sdiLogo from '../../assets/sdi1-modified.svg'
+import bismillah from '../../assets/bismillah.svg';
 
 const Header = () => {
   return (
     <div className='header-container'>
-    Testing
+      <div className="header-logo-section">
+        <img src={sdiLogo} height={'100%'}/>
+      </div>
+      <div className="header-logo-section">
+        <img src={bismillah} height={'100%'} />
+      </div>
     </div>
   );
 };
